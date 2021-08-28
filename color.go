f22ea5f7ac32d7c8b588def41e275abd6f130756
Tuomas Louekari <tuoppil@gmail.com>
2021-08-28 21:03:45 +0300

    Added toggled colors and go.mod

--- color.go
+++ color.go
@@ -3,11 +3,16 @@ package gotools
 import "fmt"
 
 const (
-	Cyan    = "\033[0;96m%s\033[0m"
-	Red     = "\033[0;91m%s\033[0m"
-	Yellow  = "\033[0;93m%s\033[0m"
-	Green   = "\033[0;92m%s\033[0m"
-	Magenta = "\033[0;95m%s\033[0m"
+	Cyan          = "\033[0;96m%s\033[0m"
+	Red           = "\033[0;91m%s\033[0m"
+	Yellow        = "\033[0;93m%s\033[0m"
+	Green         = "\033[0;92m%s\033[0m"
+	Magenta       = "\033[0;95m%s\033[0m"
+	ToggleCyan    = "\033[0;96m%s"
+	ToggleRed     = "\033[0;91m%s"
+	ToggleYellow  = "\033[0;93m%s"
+	ToggleGreen   = "\033[0;92m%s"
+	ToggleMagenta = "\033[0;95m%s"
 )
 
 // Color colors the input string
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ninjapiraatti/gotools
+
+go 1.17
